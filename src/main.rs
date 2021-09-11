c3ed3812dec4c1c423e6f6ab1cb82fabebd37a6b
vladhateganos <vladhateganos@gmail.com>
2021-09-11 18:24:37 +0300

    Created a very basic TCP echo server (telnet)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use tokio;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    loop {
+        let mut buffer = [0u8; 1024];
+        socket.read(&mut buffer).await.unwrap();
+
+        socket.write_all(&buffer[..]).await.unwrap();
+    }
+
+
 }
